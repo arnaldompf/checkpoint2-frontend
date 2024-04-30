30230386dd07297c26d345e28c3e4336a3f10a4e
diegoc85 <diegoc.spv85@gmail.com>
2024-04-30 15:26:05 -0300

    validacao do formulario JS, estilizacao do form css, alteracao input pra button html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 //Formulário
-function enviarFormulario(){
-    alert('Formulário enviado com sucesso! Em breve, entraremos em contato!');
+function validarFormulario(){
+    let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value;
+    let email = document.getElementById('email').value;
+    let mensagem = document.getElementById('message').value;
+
+    if(nome == '' || telefone == '' || email =='' || mensagem ==''){
+        alert('Por favor, preencha todos os campos');
+        return false;
+    }
+    else{
+        alert('Mensagem enviada com sucesso! Em breve, entraremos em contato!');
+        return true;
+    }
+
+
 }
