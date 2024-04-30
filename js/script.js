93f9b40054306cf8cc78d3b7dbfbba6b43e5d458
Arnaldo Filho <arnaldo.mpfilho@gmail.com>
2024-04-30 15:37:13 -0300

    age verification

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 function enviarFormulario(){
     alert('Formulário enviado com sucesso! Em breve, entraremos em contato!');
 }
+
+// Verificação Idade
+
+function liberarsite() {
+    window.location.href = "pagliberada.html"
+}
+
+function redirect() {
+    window.location.href = "https://google.com"
+}
