ec8a60dbc88694cc1d8df0e5a061327077c34b57
diegoc85 <diegoc.spv85@gmail.com>
2024-04-30 20:54:26 -0300

    Final do Trabalho

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Integrantes do grupo
 - Arnaldo Filho - RM 555780
 - Debora Ivanowski - RM 555694
 - Diego Cabral - RM 557817
+
+Observação sobre o trabalho:
+- Login de acesso está no menu superior à direita no botão 'Conta'
+- Formulário está no rodapé no link 'Formulário'
+- Harmonização baseada no vinho está na área 'Está com dúvida?'na segunda pergunta
+- A validação da idade maior de 18 está na pagina principal index.html e se sim, a resposta, vai pra pagliberada.html
+- Calculadora, cupom de desconto e adicionar vinhos ao carrinho está no menu superior à direita 'Carrinho' ou 'Sobre o produto'na área novidades. 
--- js/script.js
+++ js/script.js
@@ -37,3 +37,47 @@ function logar(){
         alert('Usuário ou senha incorretos. Tente novamente');
     }
 }
+
+function calcular(){
+    let tinto = parseInt(document.getElementById('tinto').value);
+    let branco = parseInt(document.getElementById('branco').value);
+    let rose = parseInt(document.getElementById('rose').value);
+    if(document.getElementById('sim').checked == true){
+        document.getElementById('total').innerText = (tinto*15 + branco*20 + rose*30) - (tinto*15 + branco*20 + rose*30)*0.1;
+        alert(`Cupom FIAP2024 aplicado! Valor final com desconto de R$ ${(tinto*15 + branco*20 + rose*30) - (tinto*15 + branco*20 + rose*30)*0.1}.`);
+    }
+    else{
+        alert(`Cupom NÃO APLICADO! Valor final sem desconto de R$ ${tinto*15 + branco*20 + rose*30}`);
+        document.getElementById('total').innerText = tinto*15 + branco*20 + rose*30;
+    }
+    
+    
+}
+function soma(){
+    let tinto = parseInt(document.getElementById('tinto').value);
+    let branco = parseInt(document.getElementById('branco').value);
+    let rose = parseInt(document.getElementById('rose').value);
+    document.getElementById('quantidade').innerText = tinto + branco + rose;
+    alert(`Você adicionou um total de ${tinto+branco+rose} vinhos ao seu carrinho.`) 
+}
+
+function getOption(){
+    selectElement = document.querySelector('#select1');
+    output = selectElement.value;
+    document.querySelector('.output').textContent = output; 
+
+    if(output == 'Vinho Tinto'){
+        document.querySelector('.harm_tinto').textContent = 'a) Carnes vermelhas grelhadas ou assadas, como file migon e costelas - b) Frango assado ou cozido - c) Pizza - d) Bacalhau - d) Risotos simples, como o funghi e o de queijo.';
+        document.querySelector('.harm_rose').textContent = ' ';
+        document.querySelector('.harm_branco').textContent = ' ';
+    }else if(output == 'Vinho Branco'){
+        document.querySelector('.harm_branco').textContent = 'a) Queijos brancos como brie e camembert - b) Queijos amarelos - c) peixe, frango e carne de porco';
+        document.querySelector('.harm_tinto').textContent = ' ';
+        document.querySelector('.harm_rose').textContent = ' ';
+        
+    }else{
+        document.querySelector('.harm_rose').textContent = 'a) Aves grelhadas com saladas ou, até mesmo, um strogonoff de frango - b) Carnes vermelhas bem temperadas.';
+        document.querySelector('.harm_tinto').textContent = ' ';
+        document.querySelector('.harm_branco').textContent = ' ';
+    }
+}
